c2d39586fa1f7fa8c02048a6bf199bdde539ff87
akorolev <aleksandr.korolev@t-systems.com>
2021-02-09 17:53:22 +0300

    first version

---
+++ R/hello.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param a input name
+#'
+#' @return phrase
+#' @export
+#'
+#' @examples
+#' hello("John")
+hello <- function(a) {
+  print(paste0("Hello, ", a, ", this is the world!"))
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(myRTestPrj)
+
+test_check("myRTestPrj")
